34b3bbdd10c4b59c7ba55ddf57d179bbe27ea604
John Parkhurst <jparkhurst120@gmail.com>
2025-11-10 22:09:46 -0500

    Starting

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,11 @@
+
+fn print_arr(param_arr: [i32;15]) {
+    for x in 0..param_arr.len() {
+        print!("{x} ");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let array: [i32; 15] = [0; 15];//add mut
+    print_arr(array)
 }
