b67afceaa8ece5dd16d58a5f767356c3b2c77bc9
Matt DiMeglio <mdimeglio34@gmail.com>
2025-11-11 01:07:38 -0500

    Begin Client Work
    This will create a blank template that will launch our client app for the chat server.

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use eframe::egui;
+
+fn main() -> eframe::Result<()> {
+    let options = eframe::NativeOptions::default();
+
+    eframe::run_native(
+        "Doble Technologies - Chat Application",
+        options,
+        Box::new(|_cc| Ok(Box::new(ChatApp {})))
+    )
+}
+
+struct ChatApp;
+
+impl eframe::App for ChatApp {
+    fn update(&mut self, _ctx: &egui::Context, _frame: &mut eframe::Frame) {}
 }
