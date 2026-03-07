0b0ec6c9faf4853cd049043458738158c0e947d2
John Parkhurst <jparkhurst120@gmail.com>
2026-03-07 16:36:31 -0500

    Functioning  XML beautifying

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,8 @@
 use serde_json::{json, Value,Error};// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use quick_xml::events::Event;
+use quick_xml::{Reader, Writer};
+use std::io::Cursor;
+
 pub fn json_beauty(data_str: &str) -> String {
     // Format the input string
     let value: serde_json::Value= match serde_json::from_str(data_str) {
@@ -16,17 +20,37 @@ pub fn json_beauty(data_str: &str) -> String {
     pretty
 }
 
-pub fn xml_beauty(data_str: &str) -> String {
+pub fn xml_beauty(data_str: &str) -> Result<String, quick_xml::Error> {
     // Format the input string
+    let mut reader = Reader::from_str(data_str);
+    reader.config_mut().trim_text(true); // strip existing whitespace/indentation
 
-    "pretty".to_string()
+    let mut writer = Writer::new_with_indent(Cursor::new(Vec::new()), b' ', 2);
+
+    loop {
+        match reader.read_event() {
+            Ok(Event::Eof) => break,
+            Ok(event) => writer.write_event(event)?,
+            Err(e) => return Err(e),
+        }
+    }
+
+    let bytes = writer.into_inner().into_inner();
+    Ok(String::from_utf8(bytes).expect("Valid UTF-8"))
+
 }
 
 #[tauri::command]
 fn beautify(data_str: &str, file_type: &str) -> String {
     match file_type {
         "json" => return json_beauty(data_str),
-        "xml" => return xml_beauty(data_str),
+        "xml" => {
+            // return xml_beauty(data_str)
+            return match xml_beauty(data_str) {
+                Ok(pretty) => (pretty),
+                Err(e) => (e.to_string()),
+            }
+        },
         // "yaml" | "yml" => return yaml_beauty(data_str),
         _ => data_str.to_string(),
     }
